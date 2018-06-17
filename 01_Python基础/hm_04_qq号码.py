055840a881b0e1a0e440377d8a5fe02c2b3d3144
cherrygods <the_lty_mail@foxmail.com>
2018-06-17 19:13:47 +0800

    注释\变量

---
+++ 01_Python基础/hm_04_qq号码.py
@@ -0,0 +1,8 @@
+# 定义一个QQ号码
+qq_number = '1000'
+# 定义一个QQ密码
+qq_password = '0001'
+# 　直接输出，是不可以的，在Python文件中直接写变量名是不可以输出的，但是可以在Python解释器中使用
+"账号:" + qq_number + "\n密码:" + qq_password
+# 输出到Console
+print("账号:" + qq_number + "\n密码:" + qq_password)
