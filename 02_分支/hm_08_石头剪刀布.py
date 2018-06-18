e391b45ad95de21800d690d3bdd0e408426c7f21
cherrygods <the_lty_mail@foxmail.com>
2018-06-18 17:02:52 +0800

    综合练习:石头剪刀布

---
+++ 02_分支/hm_08_石头剪刀布.py
@@ -0,0 +1,10 @@
+# 从控制台输入要输出的类型(剪刀１石头２布３)
+player = int(input("请出拳(剪刀１石头２布３):"))
+pc = 1
+print("玩家出的:%d" % player + "电脑出的:%d" % pc)
+if (player == 1 and pc == 3) or (player == 2 and pc == 1) or (player == 3 and pc == 2):
+    print("玩家胜!")
+elif player == pc:
+    print("平局")
+else:
+    print("电脑胜")
