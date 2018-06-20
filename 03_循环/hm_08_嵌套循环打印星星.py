3f12bedcaf2bb7ad7c5fafd98bd3e4472a039c63
cherrygods <the_lty_mail@foxmail.com>
2018-06-20 18:09:53 +0800

    嵌套循环实例和转义字符

--- 03_循环/hm_08_嵌套循环打印星星.py
+++ 03_循环/hm_08_嵌套循环打印星星.py
@@ -2,6 +2,9 @@ row = 1
 while row <= 5:
     col = 1
     while col <= row:
-        print("*" * col)
-        col += row
+        # 打印小星星
+        print("*", end="")
+        col += 1
+    # 在一行星星输出后添加的换行操作
+    print("")
     row += 1
