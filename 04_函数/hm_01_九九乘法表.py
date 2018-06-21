7efdd203a90a4724f8fc55d293c7401f8b019345
cherrygods <the_lty_mail@foxmail.com>
2018-06-21 17:46:17 +0800

    函数

---
+++ 04_函数/hm_01_九九乘法表.py
@@ -0,0 +1,10 @@
+def multiple_table():
+    row = 1
+    while row <= 9:
+        col = 1
+        while col <= row:
+            # print(str(col) + "*" + str(row) + "=" + str(col * row), end="\t")
+            print("%s * %s = %s" % (str(col), str(row), str(col * row)), end="\t")
+            col += 1
+        print("")
+        row += 1
---
+++ 04_函数/hm_03_第一个函数.py
@@ -0,0 +1,8 @@
+def hello_python():
+    print("Hello Python")
+    print("Hello Python")
+    print("Hello Python")
+
+
+# 函数结尾要保留两行空格
+hello_python()
