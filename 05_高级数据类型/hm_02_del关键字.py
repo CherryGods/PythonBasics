f3da05fea1fd3d557194393a27fd7abd554393aa
cherrygods <the_lty_mail@foxmail.com>
2018-06-25 01:13:02 +0800

    见文件名看功能大概吧，现在不是一文件一commit了

---
+++ 05_高级数据类型/hm_02_del关键字.py
@@ -0,0 +1,9 @@
+test_list = ["你好", "我好"]
+del test_list[0]  # 此时test_list列表中的你好就从内存中删除了
+
+test = "test"
+
+del test  # 将test变量删除，而不仅仅是删除test变量的值
+
+
+
---
+++ 05_高级数据类型/hm_03_列表的数据统计.py
@@ -0,0 +1,6 @@
+# len(length　长度)可以统计列表中元素的总数
+list1 = [1, 2, 3]
+print(len(list1))
+
+# count(count　计数) 可以统计某个数据在列表中出现的次数
+print(list1.count(2))
