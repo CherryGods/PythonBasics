fbb2cf7aede7f111fc6862ddc077d050ec7e35a5
cherrygods <the_lty_mail@foxmail.com>
2018-06-25 02:23:36 +0800

    字典的遍历

---
+++ 05_高级数据类型/hm_12_字典的遍历.py
@@ -0,0 +1,7 @@
+cherrygods = {"name": "cherrygods",
+              "qq": "2864xxx",
+              "phone": "1363xxxx"
+              }
+# 迭代遍历字典
+for x in cherrygods:
+    print("%s - %s" % (x, cherrygods[x]))
