12baf4143f945ebf465363b39b3eff8695f29093
cherrygods <the_lty_mail@foxmail.com>
2018-06-26 23:22:07 +0800

    全局变量应该所在的位置和全局变量的命名规范

--- 07_语法进阶/hm_04_修改全局变量.py
+++ 07_语法进阶/hm_04_修改全局变量.py
@@ -3,12 +3,14 @@ name = "菠萝"
 
 
 def test01():
+    # 声明这个name就是全局变量
     global name
     name = "飞机"
     print(name)
 
 
 def test02():
+    # 这里没有声明global所以是用的局部变量
     name = "香蕉皮"
     print(name)
 
