209c690396822252b83b09667622fc0712b03319
cherrygods <the_lty_mail@foxmail.com>
2018-06-28 13:25:16 +0800

    创建两个猫对象

---
+++ 08_面向对象基础/hm_02_新建两个猫对象.py
@@ -0,0 +1,17 @@
+class Cat:  # 定义猫类
+
+    def eat(self):
+        print("小猫爱吃饭")
+
+    def drink(self):
+        print("小猫爱喝水")
+
+
+# 创建猫对象
+cat = Cat()
+cat.eat()
+cat.drink()
+print(cat)
+# 再创建一个猫对象
+cat2 = Cat()
+cat.eat()
